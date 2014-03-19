83040af2f60a17d56074ee8d24ab0d1d341ce763
Steven L. Speek <slspeek@gmail.com>
2014-03-19 15:09:55 +0100

    Grunt build works

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,10 +39,10 @@ module.exports = function (grunt) {
                     livereload: true
                 }
             },
-            jstest: {
-                files: ['test/spec/{,*/}*.js'],
-                tasks: ['test:watch']
-            },
+            //jstest: {
+                //files: ['test/spec/{,*/}*.js'],
+                //tasks: ['test:watch']
+            //},
             gruntfile: {
                 files: ['Gruntfile.js']
             },
@@ -79,16 +79,16 @@ module.exports = function (grunt) {
                     ]
                 }
             },
-            test: {
-                options: {
-                    port: 9001,
-                    base: [
-                        '.tmp',
-                        'test',
-                        '<%= config.app %>'
-                    ]
-                }
-            },
+            //test: {
+                //options: {
+                    //port: 9001,
+                    //base: [
+                        //'.tmp',
+                        //'test',
+                        //'<%= config.app %>'
+                    //]
+                //}
+            //},
             dist: {
                 options: {
                     open: true,
@@ -128,14 +128,14 @@ module.exports = function (grunt) {
         },
 
         // Mocha testing framework configuration options
-        mocha: {
-            all: {
-                options: {
-                    run: true,
-                    urls: ['http://<%= connect.test.options.hostname %>:<%= connect.test.options.port %>/index.html']
-                }
-            }
-        },
+        //mocha: {
+            //all: {
+                //options: {
+                    //run: true,
+                    //urls: ['http://<%= connect.test.options.hostname %>:<%= connect.test.options.port %>/index.html']
+                //}
+            //}
+        //},
 
         // Add vendor prefixed styles
         autoprefixer: {
@@ -347,9 +347,6 @@ module.exports = function (grunt) {
         'useminPrepare',
         'concurrent:dist',
         'autoprefixer',
-        'concat',
-        'cssmin',
-        'uglify',
         'copy:dist',
         'rev',
         'usemin',
@@ -358,7 +355,6 @@ module.exports = function (grunt) {
 
     grunt.registerTask('default', [
         'newer:jshint',
-        'test',
         'build'
     ]);
 };
